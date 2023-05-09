9f3c33d8defb2657e368c65f8e5148838052ab1a
Mostafa <64909038+Mostafa-ma2@users.noreply.github.com>
2023-05-09 17:19:37 +0330

    Change SideBarHeader Style reset.css add class css helper sidebar.css change color background

--- src/Layout/SideBar/Header/SideBarHeader.jsx
+++ src/Layout/SideBar/Header/SideBarHeader.jsx
@@ -4,10 +4,10 @@ import { RiBarChartLine, RiLoginBoxLine } from 'react-icons/ri'
 export default function SideBarHeader() {
     return (
         <>
-            <div className='sidebar-header background-default d-flex p-10 color-white'>
-                <RiBarChartLine className='p-10'/>
-                <h3 className='p-10 mt-5'>Dashboard</h3>
-                <RiLoginBoxLine  className='p-10'/>
+            <div className='sidebar-header position-relative background-default d-flex p-10 color-white'>
+                <RiBarChartLine className='p-10 mt-20'/>
+                <h3 className='p-10'>Dashboard</h3>
+                <RiLoginBoxLine  className='p-10 position-absolute mt-20'/>
             </div>
         </>
     )
