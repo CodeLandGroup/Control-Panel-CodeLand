973a7bdacdd97b6abdb7ab062698a457175e14e6
Mostafa <64909038+Mostafa-ma2@users.noreply.github.com>
2023-05-09 20:24:07 +0330

    change sidebar item hover style

--- src/Layout/SideBar/SideBarMain/SideBarMain.jsx
+++ src/Layout/SideBar/SideBarMain/SideBarMain.jsx
@@ -6,22 +6,22 @@ export default function SideBarMain() {
         <>
             <div className='sidebar-main'>
                 <ul>
-                    <li className='main-item'>
-                        <a href="#" className='d-flex text-gray-2 text-center w-100 mt-20'>
+                    <li className='main-item position-relative'>
+                        <a href="#" className='transition d-flex text-gray-2 text-center w-100 mt-20'>
                             <RiBarChartLine className='w-25 text-center' />
                             <h3 className='w-50 text-center mt-5'>داشبورد</h3>
                             <RiArrowLeftSLine className='w-25 text-end' />
                         </a>
                     </li>
-                    <li className='main-item'>
-                        <a href="#" className='d-flex text-gray-2  text-center w-100 mt-20'>
+                    <li className='main-item position-relative'>
+                        <a href="#" className='transition d-flex text-gray-2  text-center w-100 mt-20'>
                             <RiGroupFill className='w-25 text-center' />
                             <h3 className='w-50 text-center mt-5'>مدیرت کاربران</h3>
                             <RiArrowLeftSLine className='w-25 text-end' />
                         </a>
                     </li>
-                    <li className='main-item'>
-                        <a href="#" className='d-flex text-gray-2 text-center w-100 mt-20'>
+                    <li className='main-item position-relative'>
+                        <a href="#" className='transition d-flex text-gray-2 text-center w-100 mt-20'>
                             <RiEqualizerFill className='w-25 text-center' />
                             <h3 className='w-50 text-center mt-5'>مدیریت پست</h3>
                             <RiArrowLeftSLine className='w-25 text-end' />
